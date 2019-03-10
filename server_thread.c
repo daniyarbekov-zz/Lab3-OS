a9afb0378ba7425abe86a7a413dcf99ef001d398
Macintosh HD <dindaniyarbekov@gmail.com>
2019-03-10 19:09:40 -0400

    added the consumer producer stuff

--- server_thread.c
+++ server_thread.c
@@ -12,6 +12,74 @@ struct server {
 
 /* static functions */
 
+
+typedef struct node {
+	int fd;
+	struct node *next;
+} node;
+
+struct wait_queue{
+	node *head;
+	node *back;
+	int size;
+};
+
+node *buildNode(int connfd)  {
+    node *temp = (node*)malloc(sizeof(node));
+	temp->fd = connfd;
+    temp->next = NULL;
+
+	return temp;
+}
+
+void enQ (int connfd, struct wait_queue *q){
+	//printf("calling build node\n");
+	node *addedNode = buildNode(connfd);
+	q->size = q->size + 1;
+
+	if(q->head == NULL){
+		q->head = addedNode;
+		q->back = addedNode;
+		return;
+	}
+	else if(q->head == q->back){
+		q->back = addedNode;
+		q->head->next = q->back;
+		return;
+	} else{
+		q->back->next = addedNode;
+		q->back = q->back->next;
+		return;
+	}
+}
+
+
+
+
+struct int poll (struct wait_queue *q){
+	
+	if(q->head == NULL){
+		return NULL;
+	}
+	else{
+		q->size = q->size - 1;
+		node *temp = q->head;
+		node *nextTemp = q->head->next;
+
+		r->head = nextTemp;
+		int connfd = temp->fd;
+		free(temp);
+
+		if(q->head == NULL){
+			q->back = NULL;
+		}
+
+		return connfd;
+	}
+}
+
+
+
 /* initialize file data */
 static struct file_data *
 file_data_init(void)
@@ -64,9 +132,16 @@ out:
 }
 
 /* entry point functions */
+struct wait_queue *q;
 
-struct server *
-server_init(int nr_threads, int max_requests, int max_cache_size)
+
+pthread_cond_t full;
+pthread_condt_t empty;
+pthread_mutex_t mutex;
+
+int numberOfActiveThreads = 0;
+
+struct server * server_init(int nr_threads, int max_requests, int max_cache_size)
 {
 	struct server *sv;
 
@@ -77,8 +152,38 @@ server_init(int nr_threads, int max_requests, int max_cache_size)
 	sv->exiting = 0;
 	
 	if (nr_threads > 0 || max_requests > 0 || max_cache_size > 0) {
-		TBD();
-	}
+		
+		pthread_cond_init(&full, NULL);
+		pthread_cond_init(&empty, NULL);
+
+		pthread_mutex_init(&mutex, NULL);
+
+
+		if(max_requests > 0){
+			q = (struct wait_queue*)malloc(sizeof(struct wait_queue));
+			q->size = 0;
+			numberOfRequests = max_requests;
+		}
+		if(nr_threads> 0){
+			arrayOfPThreads = (pthread_t **)malloc(nr_threads*sizeof(pthread_t*));
+			for(int i = 0; i < nr_threads; i++){
+				 pthread_create(arrayOfPThreads[i], NULL, consumer, sv);
+			}
+
+			// int i = 0;
+			// while(1){
+			// 	pthread_join(arrayOfPThreads[i], NULL);
+			// 	i++;
+			// 	//if condition with cond variable and lock on exit
+			// 	if(i == nr_threads){
+			// 		i = 0;
+			// 	}
+			
+			// }
+		}
+}
+
+
 
 	/* Lab 4: create queue of max_request size when max_requests > 0 */
 
@@ -89,17 +194,34 @@ server_init(int nr_threads, int max_requests, int max_cache_size)
 	return sv;
 }
 
-void
-server_request(struct server *sv, int connfd)
+
+void consumer(struct server *sv){
+	pthread_mutex_lock(&mutex);
+	while(q->size == 0){
+		pthread_cond_wait(&empty, &mutex);
+	}
+	int tfd = poll(q);
+	do_server_request(sv,tfd);
+	pthread_cond_signal(&full);
+	pthread_mutex_unlock(&mutex);
+}
+
+
+void server_request(struct server *sv, int connfd)
 {
 	if (sv->nr_threads == 0) { /* no worker threads */
 		do_server_request(sv, connfd);
 	} else {
-		/*  Save the relevant info in a buffer and have one of the
-		 *  worker threads do the work. */
-		TBD();
+		pthread_mutex_lock(&mutex);
+		while(q->size == sv->max_requests){
+			pthread_cond_wait(&full, &mutex);
+		}
+		enQ(connfd,q);
+		pthread_cond_signal(&empty);
+		pthread_mutex_unlock(&mutex);
 	}
 }
+
 
 void
 server_exit(struct server *sv)
